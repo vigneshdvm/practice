f58ce9f07c0e92bbfe556fd911de849452cf8aa5
vinay <vinay.kumbar93@gmail.com>
2022-12-02 08:40:30 +0530

    navBar created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LaptopsComponent } from './pages/laptops/laptops.component';
+import { MobilesComponent } from './pages/mobiles/mobiles.component';
+import { SneakersComponent } from './pages/sneakers/sneakers.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",component:HomeComponent},
+  {path:"home",component:HomeComponent},
+  {path:"mobiles",component:MobilesComponent},
+  {path:"laptops",component:LaptopsComponent},
+  {path:"sneakers",component:SneakersComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
